f282d7deac1306a42ca35312c6cd64e7cd2dbf26
Wu Menghao <1206919858@qq.com>
2017-11-21 16:14:48 +0800

    insert GlobalInterceptor

---
+++ src/MyJFinalIndexDemo/filter/GlobalInterceptor.java
@@ -0,0 +1,14 @@
+package MyJFinalIndexDemo.filter;
+
+import com.jfinal.aop.Interceptor;
+import com.jfinal.aop.Invocation;
+
+public class GlobalInterceptor implements Interceptor {
+
+	@Override
+	public void intercept(Invocation inv) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
