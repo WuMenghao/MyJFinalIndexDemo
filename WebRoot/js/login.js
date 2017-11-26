20b26617594645f95e8d52c5dbbbbbcd04a831f8
Wu Menghao <1206919858@qq.com>
2017-11-26 20:48:59 +0800

    11.26

--- WebRoot/js/login.js
+++ WebRoot/js/login.js
@@ -1,22 +1,99 @@
-$("document").ready(
+/*
+			 
+ * 点击事件直接出发函数
+ * */
+function checkForm() {
+	return (notNull("acount", /^\s*$/)
+			&& checkValue("acount", /^[a-zA-Z_0-9]{6,13}$/)
+			&& notNull("password", /^\s*$/) && checkValue("password",
+			/^[a-zA-Z_0-9]{6,13}$/));
 
-function() {
+}
 
+/*
+ * 
+ * 
+ * 正则验证函数
+ */
 
-	$("#submit").click(function() {
-		
-			
-		if ($("#acount").val() == "" || $("#acount").val() == null) {
-			alert("请输入用户名!");
-			$("#acount").focus();
-			return false;
-		}
+function notNull(id, regexp) {
 
-		if ($("#password").val() == "" || $("#password").val() == null) {
-			alert("请输入密码!");
-			$("#password").focus();
-			return false;
-		}
-	})
+	var Ele = document.getElementById(id);
+	var val = Ele.value;
+	var re = regexp;
 
-})
+	delectWarning(Ele);
+
+	if (re.test(val)) {
+		var ifo = Ele.previousSibling.previousSibling.innerText;
+
+		appendWarning(Ele, ifo, "不能为空!");
+
+		return false;
+
+	} else {
+		return true;
+	}
+
+}
+
+function checkValue(id, regexp) {
+
+	var Ele = document.getElementById(id);
+	var val = Ele.value;
+	var re = regexp;
+
+	delectWarning(Ele);
+
+	if (re.test(val)) {
+		return true;
+	} else {
+		var ifo = Ele.previousSibling.previousSibling.innerText;
+
+		appendWarning(Ele, ifo, "请输入6~13位!");
+
+		return false;
+	}
+}
+
+/*
+ * 
+ * 
+ * 确认两次密码
+ */
+function confirmPsw(pswID, rpswID) {
+	var Ele = document.getElementById(pswID);
+	var rEle = document.getElementById(rpswID);
+	var psw = Ele.value;
+	var rpsw = rEle.value;
+	alert(psw, rpsw);
+	if (psw == rpsw) {
+		return true;
+	} else {
+		var ifo = rEle.previousSibling.previousSibling.innerText;
+		appendWarning(rEle, ifo, "两次密码不一致!");
+		return false;
+	}
+
+}
+/*
+ * 
+ * 
+ * 提示信息添加函数
+ */
+function appendWarning(Ele, ifo, msg) {
+	var newNode = document.createElement("span");
+	var newText = document.createTextNode(ifo + msg);
+	newNode.setAttribute("style", "color: red; font-size: 12px;");
+	newNode.appendChild(newText);
+	Ele.parentNode.insertBefore(newNode, Ele.nextSibling);
+}
+/*
+ * 
+ * 
+ * 提示信息删除函数
+ */
+function delectWarning(Ele) {
+	var delectNode = Ele.nextSibling;
+	delectNode.innerText = "";
+}
--- WebRoot/view/index/login.jsp
+++ WebRoot/view/index/login.jsp
@@ -5,41 +5,19 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <link rel="stylesheet" type="text/css" href="/css/login.css">
-<script type="text/javascript" src="/js/jquery-1.8.3.min.js"></script>
 <script type="text/javascript" src="/js/login.js"></script>
 <title>登录</title>
 </head>
 <body>
-	<center>
-		<div id="loginForm">
-			<form action="/index/check" method="post" name="login">
-				<table>
-					<tr>
-						<th colspan="2">用户登录</th>
-					</tr>
-					<tr>
-						<td>账号:</td>
-						<td><div><input type="text" 
-								name="login.acount" id="acount"></div></td>
-					</tr>
-					<tr>
-						<td>密码:</td>
-						<td>
-							<input type="password" 
-								name="login.pass_word" id="password">
-						</td>
-					</tr>
-					<tr>
-						<td colspan="2">
-							<input type="submit"
-							     value="登录" id="submit"/>
-							<input 
-								type="reset" name="reset" value="重置" />
-						</td>
-					</tr>
-				</table>
-			</form>
-		</div>
-	</center>
+ 	<div id="loginForm">
+        <form action="" method="post" name="login">
+            <span class="right">账号:</span>
+            <input type="text" name="login,acount" id="acount"><br>
+            <span class="right">密码:</span>
+            <input type="password" name="login.pass_word" id="password" ><br>
+            <input type="submit" value="登录" id="submit" onclick="return checkForm();"/>
+            <input type="reset" name="reset" value="重置" />
+        </form>
+    </div>
 </body>
 </html>
